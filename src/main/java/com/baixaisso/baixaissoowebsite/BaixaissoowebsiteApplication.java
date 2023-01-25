6f3b7140444101b7a734223afccc2bc717273df9
Matheus <matheusmaniero@gmail.com>
2023-01-25 10:04:13 -0300

    First Commit

---
+++ src/main/java/com/baixaisso/baixaissoowebsite/BaixaissoowebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.baixaisso.baixaissoowebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaixaissoowebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaixaissoowebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baixaisso/baixaissoowebsite/BaixaissoowebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baixaisso.baixaissoowebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaixaissoowebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
